6e8bacafd134a0e2225ccbc13cd7f9a04332ba53
Kameron Ahler <kamahler@gmail.com>
2020-08-26 12:43:43 -0600

    add Button component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 export default function App() {
-  return <div></div>
+  return (
+    <div>
+      <Button modifier={'primary'} text={'Test'} />
+    </div>
+  )
 }
